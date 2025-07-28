b990e9df50660bda8ccc82152be045bed29927cc
Kristina Carter <carter_kristina@proton.me>
2025-07-28 11:28:10 -0400

    Added todo list app

---
+++ js/11-todo-list.js
@@ -0,0 +1,39 @@
+const todoList = [];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const { name, dueDate } = todoList[i];
+
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Remove</button>
+    `;
+
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  if (name.trim() !== '') {
+    todoList.push({ name, dueDate });
+    inputElement.value = '';
+    dateInputElement.value = '';
+    renderTodoList();
+  }
+}
